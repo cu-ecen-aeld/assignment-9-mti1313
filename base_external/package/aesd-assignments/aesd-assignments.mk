0ec62285e578b595ca6768a57f35154fff4e4f46
markizzo <markizzo@Ubuntu.myguest.virtualbox.org>
2023-10-13 14:51:51 -0600

    update finder

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '54bd2f3b8145d8e325883b035f2f40564c5b1c63'
+AESD_ASSIGNMENTS_VERSION = '3d7718324c50e5a22d320596de66d75f3591509e'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
